ff2e41b3a8f28bffa0a5c343f801d486b0064d27
mcmcgrath13 <m.c.mcgrath13@gmail.com>
2019-10-08 09:37:27 -0400

    test: remove image test, images not in provocation utils

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -9,10 +9,6 @@ describe('generic utility functions', function() {
     assert.closeTo(util.jitter50(100), 100, 50, 'jitter50 is 50 within 100')
   })
 
-  it('should load images', function() {
-    assert.lengthOf(util.images, 2, 'there are two images in the assets folder')
-  })
-
   it('should flip a coin', function() {
     assert.isBoolean(util.randomTrue(), 'randomTrue is true or false')
     const flips = _.range(100).map( (val) => util.randomTrue() )
