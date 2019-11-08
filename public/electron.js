9a0463f2f6130f73c7dfb996724cdbfbf9733f39
mcmcgrath13 <m.c.mcgrath13@gmail.com>
2019-11-08 15:57:35 -0500

    ci: revert releasify in wf, add squirrel-startup to electron.js

--- public/electron.js
+++ public/electron.js
@@ -1,3 +1,6 @@
+// handle windows installer set up
+if(require('electron-squirrel-startup')) return
+
 // Modules to control application life and create native browser window
 const { app, BrowserWindow, dialog } = require('electron')
 const path = require('path')
