918b9726a23f494f3f6eb03f1ce10108bf8e74d8
mcmcgrath13 <m.c.mcgrath13@gmail.com>
2019-11-08 13:44:42 -0500

    ci: move action to repo

--- .github/actions/get-package-info/main.js
+++
@@ -1,8 +0,0 @@
-const core = require('@actions/core');
-const package = require('../../../package.json')
-
-const package_version = package.version
-const package_name = package.name
-
-core.setOutput('package_version', package_version)
-core.setOutput('package_name', package_name)
